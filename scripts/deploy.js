437bc097fbcc1172d6fa50081ada0fdba01c478c
Mikers <snissn@gmail.com>
2024-09-11 13:45:13 -1000

    simple mcopy test that uses assembly

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,18 +5,25 @@ async function main() {
   await hre.run('compile');
 
   // Deploy the contract
-  const ContractFactory = await hre.ethers.getContractFactory("MyToken");
+  const ContractFactory = await hre.ethers.getContractFactory("MCOPYTest");
   const contract = await ContractFactory.deploy();
   await contract.deployed();
 
   console.log(`Contract deployed to address: ${contract.address}`);
 
   try {
-    // Attempt to call the `name` function
-    const contractName = await contract.name();
-    console.log(`Contract name: ${contractName}`);
+    // Test data
+    //const testData = hre.ethers.utils.formatBytes32String("Test Data");
+    const testData = "0x48656c6c6f20576f726c64210000000000000000000000000000000000000000"; // hello world
+
+    // Call the optimizedCopy function
+    const result = await contract.optimizedCopy(hre.ethers.utils.arrayify(testData));
+
+    console.log("Original Data:", testData);
+    console.log("Copied Data:", hre.ethers.utils.parseBytes32String(result));
+
   } catch (error) {
-    console.error("Error calling `name` function on deployed contract:");
+    console.error("Error calling `optimizedCopy` function on deployed contract:");
     console.error(error);
   }
 }
